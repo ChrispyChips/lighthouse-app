f75ea1a3c3e9d9477877865f899635fecff1710c
Chris <chrismpretorius@gmail.com>
2019-03-16 23:01:43 +0200

    wipe

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
-import { cube } from './math.js';
-
-if (process.env.NODE_ENV !== 'production') {
-   console.log('Looks like we are in development mode!');
-}
-
 function component() {
+  let element = document.createElement('div');
 
-  var element = document.createElement('div');
-
-  element.innerHTML = [
-   'Hello webpack!',
-   '5 cubed is equal to ' + cube(5)
-  ].join('\n\n');
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
 }
- document.body.appendChild(component());
+
+document.body.appendChild(component());
