4667992fe394e9fd51ef5722b3d34b050c870de2
Chris <chrismpretorius@gmail.com>
2019-03-17 02:18:36 +0200

    build multiple pages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import _ from 'lodash';
+import printMe from './print.js';
 
 function component() {
-  let element = document.createElement('div');
+    var element = document.createElement('div');
+    var btn = document.createElement('button');
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  return element;
-}
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+
+    return element;
+  }
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,53 @@
 const path = require('path');
+const fs = require('fs')
+
+//Array of pages in ./src/pages
+const pages = fs.readdirSync(path.resolve(__dirname, 'src/pages')).filter(fileName => fileName.endsWith('.html'));
+
+//Trigger HtmlWebpackPlugin for each page 
+function generateHtmlPlugins (templateDir) {
+  const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+  const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir))
+  return templateFiles.map(item => {
+    // Split names and extension
+    const parts = item.split('.')
+    const name = parts[0]
+    const extension = parts[1]
+    return new HtmlWebpackPlugin({
+      filename: `${name}.html`,
+      template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`)
+    })
+  })
+}
+
+const htmlPlugins = generateHtmlPlugins('./src/pages');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+   app: './src/index.js',
+   print: './src/print.js'
+  },
+  plugins: [].concat(htmlPlugins),
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+     rules: [
+       {
+         test: /\.css$/,
+         use: [
+           'style-loader',
+           'css-loader'
+         ]
+       },
+       {
+         test: /\.(png|svg|jpg|gif)$/,
+         use: [
+           'file-loader'
+         ]
+       }
+     ]
+   }
 };
