30521f59386a0b788d9029cb01760e8b8751895e
Chris <chrismpretorius@gmail.com>
2019-03-19 23:33:42 +0200

    docs build

---
+++ docs/precache-manifest.28ef827f06c9a4dac6549b7a1c97ec7f.js
@@ -0,0 +1,20 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff17f955fadfe0657a21069d41b375eb",
+    "url": "test.html"
+  },
+  {
+    "url": "test.da9e0cccf555ca9d9876.js"
+  },
+  {
+    "revision": "ede1f38812ab22511937a7ad44c030cf",
+    "url": "index.html"
+  },
+  {
+    "url": "index.da9e0cccf555ca9d9876.js"
+  },
+  {
+    "revision": "3bfd63ad38d08634e5e70d1ec3bd60c4",
+    "url": "assets/favicon.png"
+  }
+]);
