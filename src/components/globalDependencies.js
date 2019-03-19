dc26eb2dba0423bbe061590d1bb102238b24e480
Chris <chrismpretorius@gmail.com>
2019-03-19 23:05:46 +0200

    Progressive webapp service worker in production build

--- src/components/globalDependencies.js
+++ src/components/globalDependencies.js
@@ -2,3 +2,16 @@ require("jquery");
 require('bootstrap');
 require('popper.js');
 require('bootstrap/scss/bootstrap.scss');
+
+if (process.env.NODE_ENV === 'production') {
+  //Registers service workers when production build
+  if ('serviceWorker' in navigator) {
+   window.addEventListener('load', () => {
+     navigator.serviceWorker.register('/service-worker.js').then(registration => {
+       console.log('SW registered: ', registration);
+     }).catch(registrationError => {
+       console.log('SW registration failed: ', registrationError);
+     });
+   });
+  }
+}
