da3e1e7008d893002997239a9e23e4118bbd1361
Chris <chrismpretorius@gmail.com>
2019-03-20 01:22:30 +0200

    build docs folder for viewing in github pages

---
+++ docs/precache-manifest.0cd62aec683f2db82b72e50d6c0c41f2.js
@@ -0,0 +1,20 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5b679a4d217c767e3e31d9f17d0983d",
+    "url": "test.html"
+  },
+  {
+    "url": "test.48949b55c8bd254aa2dc.js"
+  },
+  {
+    "revision": "fbb649cfcdc4c7a6a02978c3281e6f60",
+    "url": "index.html"
+  },
+  {
+    "url": "index.48949b55c8bd254aa2dc.js"
+  },
+  {
+    "revision": "3bfd63ad38d08634e5e70d1ec3bd60c4",
+    "url": "assets/favicon.png"
+  }
+]);
