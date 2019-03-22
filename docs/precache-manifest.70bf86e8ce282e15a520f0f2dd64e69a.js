67e910f2b1dc638b106cd63aee0dd0a1d6567ab5
Chris <chrismpretorius@gmail.com>
2019-03-22 23:20:22 +0200

    navbar

--- docs/precache-manifest.70bf86e8ce282e15a520f0f2dd64e69a.js
+++ docs/precache-manifest.70bf86e8ce282e15a520f0f2dd64e69a.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "af9aa84c09432ec8aa9c951268ada3af",
+    "revision": "930fd702c3d0b8b1adc925013c6a0ad0",
     "url": "test.html"
   },
   {
-    "url": "test.a99ce75ac2ecd700ab0d.js"
+    "url": "test.03b601f581b90af4a964.js"
   },
   {
-    "url": "test.a99ce75ac2ecd700ab0d.css"
+    "url": "test.03b601f581b90af4a964.css"
   },
   {
     "revision": "4c98e42b088d9d5c231676293fbc1046",
     "url": "manifest.json"
   },
   {
-    "revision": "56f4a33832b7be6ccc84e8890592a5ff",
+    "revision": "9c9de545e14ab2c33d88ca0d3eb5862b",
     "url": "index.html"
   },
   {
-    "url": "index.a99ce75ac2ecd700ab0d.js"
+    "url": "index.03b601f581b90af4a964.js"
   },
   {
-    "url": "index.a99ce75ac2ecd700ab0d.css"
+    "url": "index.03b601f581b90af4a964.css"
   },
   {
     "revision": "93cd25f478365d7d0206ec262d4a2eb1",
--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.c390ceb626bb1579c647ccf6134c20d7.js", "https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox-sw.js");
+importScripts("precache-manifest.70bf86e8ce282e15a520f0f2dd64e69a.js", "https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox-sw.js");
 
 console.log('Hello service worker');
 
